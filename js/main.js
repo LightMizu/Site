d40a8eb7be8795385e33c6f80e77eccacb03ab89
LightMizu <1blend.your.life1@gmail.com>
2022-12-10 13:10:38 +0300

    add button func

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.navigation')
-
+const auth_btn = document.querySelector('.auth_btn')
+const auth_window = document.querySelector('.container')
 burger.addEventListener('click', () => {
   nav.classList.toggle('active')
+})
+
+auth_btn.addEventListener('click', () => {
+  auth_window.classList.toggle('active')
 })
