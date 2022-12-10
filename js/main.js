9730ad0ccff29ccb68c3a9666dd327f5b591acd2
LightMizu <1blend.your.life1@gmail.com>
2022-12-11 01:57:40 +0300

    Add reg and auth windows and def for this

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.navigation')
 const auth_btn = document.querySelector('.auth_btn')
-const auth_window = document.querySelector('.container')
-const close = document.querySelector('.close')
+const auth_window = document.querySelector('.auth')
+const auth_close = document.querySelector('.auth .close')
+const reg_btn = document.querySelector('.no_acc')
+const reg_window = document.querySelector('.register')
+const reg_close = document.querySelector('.register .close')
 burger.addEventListener('click', () => {
   nav.classList.toggle('active')
 })
@@ -11,6 +14,15 @@ auth_btn.addEventListener('click', () => {
   auth_window.classList.toggle('active')
 })
 
-close.addEventListener('click', () => {
+auth_close.addEventListener('click', () => {
   auth_window.classList.toggle('active')
+})
+
+reg_btn.addEventListener('click', () => {
+  auth_window.classList.toggle('active')
+  reg_window.classList.toggle('active')
+})
+
+reg_close.addEventListener('click', () => {
+  reg_window.classList.toggle('active')
 })
