bc1a90475a2197ae9ed3757a758d9059afdcd84f
TIHOUN <pacman9914@mail.ru>
2022-12-10 17:39:44 +0300

    Add def on click button

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ const burger = document.querySelector('.burger')
 const nav = document.querySelector('.navigation')
 const auth_btn = document.querySelector('.auth_btn')
 const auth_window = document.querySelector('.container')
+const close = document.querySelector('.close')
 burger.addEventListener('click', () => {
   nav.classList.toggle('active')
 })
 
 auth_btn.addEventListener('click', () => {
+  auth_window.classList.toggle('active')
+})
+
+close.addEventListener('click', () => {
   auth_window.classList.toggle('active')
 })
